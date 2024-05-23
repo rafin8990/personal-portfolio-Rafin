f0275421fc20f2dd63a4bf6f4c5839165c054d50
rafin8990 <rafin8990@gmail.com>
2024-05-23 17:50:03 +0600

    code added

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import { FcHome } from "react-icons/fc";
 
 const Navbar = () => {
   return (
     <div className="">
-      
+      <ul className="social-icons">
+        <li><Link  className="social-button text-2xl" href=""><FcHome className="icon" /> <span className="text">Home</span> </Link></li>
+      </ul>
     </div>
   );
 };
