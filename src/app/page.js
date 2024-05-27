70184fc89c0b9d95c0fe890ea52e86ae1601e730
M.H. Rashed <the.mh.rashed@gmail.com>
2024-05-27 21:06:19 +0600

    work experience added

--- src/app/page.js
+++ src/app/page.js
@@ -13,25 +13,6 @@ import profile from "../../public/Assets/PROFILE.png";
 import Baits from "../../public/logo/BAITS.png";
 import Timeline from "@/Components/HomeComponent/Timeline";
 
-const experiences = [
-  {
-    companyName: "Bangladesh Associate Of IT Solutions",
-    position: "Software Developer",
-    duration: "November 2023 - Present",
-    technologies:
-      "PHP, Laravel, Node JS, Express JS , React, Next JS, MongoDB, MySQL, PostgreSQL",
-    address:
-      " Alba Tower , 6th floor , Pallabi, Mirpur , Dhaka Bangladesh (Onsite)",
-  },
-  {
-    companyName: "Swachh Akshar(Inginious Solutions)",
-    position: "MERN Stack Developer",
-    duration: "February 2023 - October 2023",
-    technologies:
-      "Node JS, Express JS , React, Next JS, MongoDB, MySQL, PostgreSQL",
-    address: " 145, Zone 2, GRP Colony, MP Nagar, Bhopal, MP -4620001 (Remote)",
-  },
-];
 export default function Home() {
   const logos = [
     { src: "/logo/React.webp", alt: "React Logo" },
@@ -59,7 +40,7 @@ export default function Home() {
         <div className="order-1 p-2">
           <h1 className="text-4xl text-white">HELLO</h1>
           <h1 className="text-4xl md:text-6xl text-white">
-            I am Rafin Hossain{" "}
+            Meet Rafin Hossain{" "}
           </h1>
 
           <TypeAnimation
@@ -87,9 +68,11 @@ export default function Home() {
           <div>
             <Link href="/about">
               {" "}
-              <button className="button2">More About Me</button>
+              <button className="button2 text-black">More About Me</button>
             </Link>
-            <button className="button2 mx-4 ">Download Resume</button>
+            <button className="button2 mx-4 text-black ">
+              Download Resume
+            </button>
           </div>
           <div className="flex gap-5 mt-3">
             <Link
@@ -123,10 +106,10 @@ export default function Home() {
         </div>
       </div>
       <div className="mt-20 pb-10">
-        <h5 className="text-xl text-gray-300 text-center">
+        <h5 className="text-xl text-black text-center">
           What I Have Done So Far
         </h5>
-        <h1 className="text-5xl text-center font-bold text-white mt-2">
+        <h1 className="text-3xl md:text-5xl text-center font-bold text-white mt-2">
           ____Work Experiences____
         </h1>
       </div>
